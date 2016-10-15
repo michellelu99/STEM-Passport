9ff9340f053f533158c9fd471656302d201497f5
Michelle Lu <michellelu.99@gmail.com>
2016-10-15 17:49:09 -0400

    Timeline stuff

--- javascript.js
+++ javascript.js
@@ -7,239 +7,68 @@ function validate() {
     }
 }
 
-(function() {
+var items = document.querySelectorAll(".timeline li");
 
-  'use strict';
-
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
-
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  
   function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
-    }
+     items[0].classList.add("in-view");
   }
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-
-})();
-
-/*(function() {
-  var questions = [{
-    question: "What is 2*5?",
-    choices: [2, 5, 10, 15, 20],
-    correctAnswer: 2
-  }, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
-  }, {
-    question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
-    correctAnswer: 0
-  }, {
-    question: "What is 1*7?",
-    choices: [4, 5, 6, 7, 8],
-    correctAnswer: 3
-  }, {
-    question: "What is 8*8?",
-    choices: [20, 30, 40, 50, 64],
-    correctAnswer: 4
-  }];
-  
-  var questionCounter = 0; //Tracks question number
-  var selections = []; //Array containing user choices
-  var quiz = $('#quiz'); //Quiz div object
-  
-  // Display initial question
-  displayNext();
-  
-  // Click handler for the 'next' button
-  $('#next').on('click', function (e) {
-    e.preventDefault();
-    
-    // Suspend click listener during fade animation
-    if(quiz.is(':animated')) {        
-      return false;
-    }
-    choose();
-    
-    // If no user selection, progress is stopped
-    if (isNaN(selections[questionCounter])) {
-      alert('Please make a selection!');
-    } else {
-      questionCounter++;
-      displayNext();
-    }
-  });
-  
-  // Click handler for the 'prev' button
-  $('#prev').on('click', function (e) {
-    e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    choose();
-    questionCounter--;
-    displayNext();
-  });
-  
-  // Click handler for the 'Start Over' button
-  $('#start').on('click', function (e) {
-    e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    questionCounter = 0;
-    selections = [];
-    displayNext();
-    $('#start').hide();
-  });
-  
-  // Animates buttons on hover
-  $('.button').on('mouseenter', function () {
-    $(this).addClass('active');
-  });
-  $('.button').on('mouseleave', function () {
-    $(this).removeClass('active');
-  });
-  
-  // Creates and returns the div that contains the questions and 
-  // the answer selections
-  function createQuestionElement(index) {
-    var qElement = $('<div>', {
-      id: 'question'
-    });
-    
-    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
-    qElement.append(header);
-    
-    var question = $('<p>').append(questions[index].question);
-    qElement.append(question);
-    
-    var radioButtons = createRadios(index);
-    qElement.append(radioButtons);
-    
-    return qElement;
+function callbackFunc1() {
+     items[1].classList.add("in-view");
   }
-  
-  // Creates a list of the answer choices as radio inputs
-  function createRadios(index) {
-    var radioList = $('<ul>');
-    var item;
-    var input = '';
-    for (var i = 0; i < questions[index].choices.length; i++) {
-      item = $('<li>');
-      input = '<input type="radio" name="answer" value=' + i + ' />';
-      input += questions[index].choices[i];
-      item.append(input);
-      radioList.append(item);
-    }
-    return radioList;
+
+function callbackFunc2() {
+     items[2].classList.add("in-view");
   }
-  
-  // Reads the user selection and pushes the value to an array
-  function choose() {
-    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+
+function callbackFunc3() {
+     items[3].classList.add("in-view");
   }
-  
-  // Displays next requested element
-  function displayNext() {
-    quiz.fadeOut(function() {
-      $('#question').remove();
-      
-      if(questionCounter < questions.length){
-        var nextQuestion = createQuestionElement(questionCounter);
-        quiz.append(nextQuestion).fadeIn();
-        if (!(isNaN(selections[questionCounter]))) {
-          $('input[value='+selections[questionCounter]+']').prop('checked', true);
-        }
-        
-        // Controls display of 'prev' button
-        if(questionCounter === 1){
-          $('#prev').show();
-        } else if(questionCounter === 0){
-          
-          $('#prev').hide();
-          $('#next').show();
-        }
-      }else {
-        var scoreElem = displayScore();
-        quiz.append(scoreElem).fadeIn();
-        $('#next').hide();
-        $('#prev').hide();
-        $('#start').show();
-      }
-    });
+
+function callbackFunc4() {
+     items[4].classList.add("in-view");
   }
-  
-  // Computes score and returns a paragraph element to be displayed
-  function displayScore() {
-    var score = $('<p>',{id: 'question'});
-    
-    var numCorrect = 0;
-    for (var i = 0; i < selections.length; i++) {
-      if (selections[i] === questions[i].correctAnswer) {
-        numCorrect++;
-      }
-    }
-    
-    score.append('You got ' + numCorrect + ' questions out of ' +
-                 questions.length + ' right!!!');
-    return score;
+
+function callbackFunc5() {
+     items[5].classList.add("in-view");
   }
-})();
 
-var questionArr=new Array("What is Chien-Shiung Wu's most famous experiment nicknamed?","What was the name of the textbook Wu wrote?", 
-	"What field is Chien-Shiung Wu in?","Chien-Shiung Wu was the first female president of what society?",
-	"What is the name of the asteroid named after her?");
+function callbackFunc6() {
+     items[6].classList.add("in-view");
+  }
 
-var answerArr=new Array("Wu Experiment","Beta Decay","Nuclear Physics","American Physical Society","2752 Wu Chien-Shiung");
+function callbackFunc7() {
+     items[7].classList.add("in-view");
+  }
 
-var arrayLength = questionArr.length;
+function callbackFunc8() {
+     items[8].classList.add("in-view");
+  }
 
-function startButtonClicked() {
-	
-	for(i=0;i<arrayLength;i++){     	
-		
-		var answer=prompt(questionArr[i]);
+function callbackFunc9() {
+     items[9].classList.add("in-view");
+  }
 
-		if(answer.localeCompare(answerArr[i]) == 0){
- 			var p=document.getElementById("answers");
- 			p.innerHTML = questionArr[i];
- 			var li=document.createElement("li"); 	
-			li.innerHTML="Correct";
- 			var ul=document.getElementById("rightwrong");
-			ul.appendChild(li); 
-		}
-		
-		else{
- 			var p=document.getElementById("answers");
- 			p.innerHTML = questionArr[i];
- 			var li=document.createElement("li"); 	
-			li.innerHTML="Incorrect";
- 			var ul=document.getElementById("rightwrong");
-			ul.appendChild(li); 
-		}
-	} 	
-}/*
+function callbackFunc10() {
+     items[10].classList.add("in-view");
+  }
+
+function callbackFunc11() {
+     items[11].classList.add("in-view");
+  }
+
+function callbackFunc12() {
+     items[12].classList.add("in-view");
+  }
+
+function callbackFunc13() {
+     items[13].classList.add("in-view");
+  }
+
+function callbackFunc14() {
+     items[14].classList.add("in-view");
+  }
+
+function callbackFunc15() {
+     items[15].classList.add("in-view");
+  }
